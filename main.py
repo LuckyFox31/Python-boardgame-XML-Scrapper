a25c73f6e2cf5bb39571b90eb98405833af71408
LuckyFox <noah.grebet@orange.fr>
2022-12-04 12:49:05 +0100

    :sparkles: Create routes file

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-# main.py
+# -- Modules --
+from bs4 import BeautifulSoup
+import requests
+import json
+from flask import Flask
+import src.routes as routes
+
+
---
+++ src/routes.py
@@ -0,0 +1,3 @@
+base_url = 'https://api.geekdo.com/xmlapi'
+collection_route = '/collection'
+boardgame_route = '/boardgame'
